bfa9433eb779906de6fa1b20dbeae0e31ecfaf22
1p22geo <1p22geodecki@gmail.com>
2024-05-25 20:53:33 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatIcon } from '@angular/material/icon';
+import { MatButton, MatFabButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIcon,
+    MatButton,
+    MatFabButton,
+    MatIcon,
+    MatToolbar,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    TodoListComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-todo';
---
+++ src/app/components/add-todo/add-todo.component.ts
@@ -0,0 +1,43 @@
+import { Component, Output, OutputEmitterRef } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { Todo } from '../../lib/todo';
+
+@Component({
+  selector: 'app-add-todo',
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButton,
+    MatButtonModule,
+  ],
+  templateUrl: './add-todo.component.html',
+  styleUrl: './add-todo.component.scss',
+})
+export class AddTodoComponent {
+  @Output() addTodo = new OutputEmitterRef<Todo>();
+  title = new FormControl('');
+  desc = new FormControl('');
+  date = new FormControl('');
+  clearForm() {
+    this.title.setValue('');
+    this.desc.setValue('');
+    this.date.setValue('');
+  }
+  submitForm() {
+    if (!this.title.value) return
+    if (!this.date.value) return
+    this.addTodo.emit({
+      title: this.title.value,
+      content: this.desc.value,
+      date: new Date(this.date.value)
+    })
+    this.clearForm()
+  }
+}
